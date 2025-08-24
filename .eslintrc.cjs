5da2a88558f5d24c63b076fe47f73ad2a48ea4d0
sirhCC <ccharles199220@gmail.com>
2025-08-23 22:52:01 -0400

    feat: add ESLint configuration for TypeScript support

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { sourceType: 'module', ecmaVersion: 'latest' },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+};
